b58e476e09fe9c8c9f856d925ffa5bdbe8235f8d
AshishNevan <nevchris242@gmail.com>
2021-09-27 20:06:04 +0530

    implemented roll '1' , player switch functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -9,12 +11,15 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 //initial values
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-let currentScore = 0;
+// let currentScore = 0;
 
 // dice roll funtionality
 btnRoll.addEventListener('click', function () {
@@ -29,7 +34,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    activePlayer = ++activePlayer % 2;
   }
 });
